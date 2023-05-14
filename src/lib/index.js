85135933c0ad13cf33ec41edb3fca9f50aec6bf3
truefusion <truefusion@gmail.com>
2023-05-14 05:58:42 -0400

    Update index.js
    Bug fix.

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,7 +2,7 @@ export { default as Image } from './shapes/SkittleImage';
 export { default as ImageCache } from './ImageCache';
 export { default as Layer } from './Skittle';
 export { default as Rect } from './shapes/SkittleRect';
-export { default as Renderer } from './shapes/SkittleRect';
+export { default as Renderer } from './SkittleRenderer';
 export { default as Shape } from './shapes/SkittleShape';
 export { default as StyledShape } from './shapes/SkittleStyledShape';
 
