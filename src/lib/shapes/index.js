9447e9f92c776faaac88fc9d5927e719f3fcf4d8
truefusion <truefusion@gmail.com>
2023-05-28 18:25:02 -0400

    Refactoring.
    Exports circle.

---
+++ src/lib/shapes/index.js
@@ -0,0 +1,5 @@
+export { default as Circle } from './SkittleCircle';
+export { default as Image } from './SkittleImage';
+export { default as Rect } from './SkittleRect';
+export { default as Shape } from './SkittleShape';
+export { default as StyledShape } from './SkittleStyledShape';
