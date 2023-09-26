4c385b4f9742c4046d47f173e05299485ea95880
truefusion <truefusion@gmail.com>
2023-09-26 07:02:51 -0400

    Docs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,27 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import emoji from 'remark-emoji';
 import remarkDirective from 'remark-directive';
-
 import fileInclude from './docs/plugins/file-include.js';
 import note from './docs/plugins/note.js';
+import mdx from "@astrojs/mdx";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
 	outDir: './docs/dist',
 	srcDir: './docs',
-	integrations: [tailwind({
-		applyBaseStyles: false,
-	})],
+	integrations: [
+		tailwind({
+			applyBaseStyles: false
+		}),
+		mdx(),
+		vue(),
+	],
 	markdown: {
-		remarkPlugins: [
-			emoji,
-			fileInclude,
-			remarkDirective,
-			note,
-		],
+		remarkPlugins: [emoji, fileInclude, remarkDirective, note],
 		shikiConfig: {
-			theme: 'material-theme',
+			theme: 'material-theme'
 		},
 	},
 });
