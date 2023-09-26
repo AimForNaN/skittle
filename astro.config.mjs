96458b1531ba3057e468814f37070e60dd39ce74
truefusion <truefusion@gmail.com>
2023-09-26 09:28:34 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import vue from "@astrojs/vue";
 import emoji from 'remark-emoji';
 import remarkDirective from 'remark-directive';
 import fileInclude from './docs/plugins/file-include.js';
 import note from './docs/plugins/note.js';
 import mdx from "@astrojs/mdx";
-
-import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
